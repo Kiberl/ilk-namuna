0c8155315b73e0013b8f9aa8d8760d2dfb37eb1c
Kiberl <161002251+Kiberl@users.noreply.github.com>
2024-02-23 12:19:19 +0500

    ilk kuylanma
    davomiy sonlarning yig'indisi uchun hizmat qiladi

---
+++ davomiylar yigindisi.py
@@ -0,0 +1,11 @@
+A=int(input('1 dan kiritgan soninggizga qadar bo\'lgan sonlar yig\'indisini chiqaradi___'))
+j1=A/2
+A2=A+1
+###########__toqlik
+j2=A-1
+j3=j2/2
+A3=A*j3+A
+if A%2==0:
+    print(A2*j1)
+else:
+    print(A3)
